ed679846fd3078218252e16670e6da0b4815739c
Ashok Thakur <iashok180@gmail.com>
2019-06-01 00:00:53 +0530

    scrapper added

--- process.py
+++ process.py
@@ -1,3 +1,4 @@
+from bs4 import BeautifulSoup
 import pandas as pd
 import json
 
@@ -5,3 +6,9 @@ def read_data():
     df = pd.read_csv('static/data/data.csv')
     df = df.to_json(orient='records')
     return df
+
+def extract_data():
+    url = 'https://timesofindia.indiatimes.com/elections/results'
+    soup = BeautifulSoup(url, 'lxml')
+    test = soup.find("div", property="id:stateBody")
+    return test
